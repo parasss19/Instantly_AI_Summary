91e8bd2f293a9cf7b13e899fa708cd2a6385b2a3
Paras <mehtaparas1901@gmail.com>
2025-04-21 19:58:52 +0530

    viteconfig file updated

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react()
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),       // main entry (could be popup)
+        options: resolve(__dirname, 'options.html')   // options page
+      }
+    },
+    outDir: 'dist',      // default is dist, can be changed if needed
+    emptyOutDir: true    // clean output dir before each build
+  }
 })
