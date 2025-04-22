a05b963ef380fa7360436fb4007ddabe46419049
Paras <mehtaparas1901@gmail.com>
2025-04-22 17:43:12 +0530

    background script updated

--- public/scripts/background.js
+++ public/scripts/background.js
@@ -1,7 +1,7 @@
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.get(["geminiAPIKey"], (result) => {
         if(!result.geminiAPIKey){
-            chrome.tabs.create({url : "options.html"});
+            chrome.tabs.create({ url: chrome.runtime.getURL("index.html#/options") });
         }
     })
 })
