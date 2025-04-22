215a3244f58bb890a156a248d0d6345fb4f5ea36
Paras <mehtaparas1901@gmail.com>
2025-04-22 17:43:37 +0530

    Options page added

--- src/options/Options.jsx
+++ src/options/Options.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { GoLink } from "react-icons/go";
 
 const Options = () => {
   const [key, setKey] = useState("");
@@ -27,42 +26,50 @@ const Options = () => {
   }
 
   return (
-    <>
-      <h2 className="">AI Summary Settings</h2>
+    <div className="p-3 w-[380px] bg-amber-100">
+      <h2 className="font-[poppins] text-2xl font-semibold mb-2">Set ApiKey</h2>
 
       {/* input field for api key */}
-      <div className="">
-        <label className="">Gemini Api Key</label>
+      <div className="w-full flex gap-2 font-mono mb-3">
+        <label className="font-mono text-lg font-bold">Gemini Api Key</label>
         <input
           type="text"
           value={key}
           id="api-key"
           onChange={(e) => setKey(e.target.value)}
           placeholder="Enter your Gemini API key"
-          className=""
+          className="w-[55%] border outline-none px-2 rounded text-black"
         />
       </div>
 
-      {/* get api key link */}
-      <div>
-        <p className="">
-          Get Your API Key from{" "}
-          <a
-            href="https://makersuite.google.com/app/apikey"
-            className=""
-            target="blank"
-          >
-            Link <GoLink />
-          </a>
-        </p>
-      </div>
-
       {/* save btn and message */}
-      <div>
-        <button onClick={handleSave} className="" >Save Settings</button>
+      <div className="mb-5">
+        <button 
+          onClick={handleSave} 
+          className="px-2 py-1 border text-md bg-red-300 rounded outline-none cursor-pointer" 
+        > Save Key
+        </button>
+
         {message && <p className="text-green-600">{message}</p>}
       </div>
-    </>
+
+      <hr className="h-3 w-[97%] text-gray-600" />
+
+      {/* get api key link */}
+      <div className="flex gap-2 items-center">
+          <p className="font-mono text-lg text-red-500 font-bold">Don't have key </p>
+          <button className="px-2 py-1 border text-md bg-red-300 rounded outline-none cursor-pointer">
+            <a
+              href="https://makersuite.google.com/app/apikey"
+              className=""
+              target="blank"
+            >
+            Generate Key
+            </a>
+          </button>
+      </div>
+
+    </div>
   );
 };
 
