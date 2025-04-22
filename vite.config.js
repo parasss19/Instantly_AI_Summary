c408d21a109d545b32d6f01590968aba8d851ee7
Paras <mehtaparas1901@gmail.com>
2025-04-22 17:42:03 +0530

    removed options.html

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [
@@ -9,13 +8,7 @@ export default defineConfig({
     react()
   ],
   build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),       // main entry (could be popup)
-        options: resolve(__dirname, 'options.html')   // options page
-      }
-    },
-    outDir: 'dist',      // default is dist, can be changed if needed
-    emptyOutDir: true    // clean output dir before each build
+    outDir: 'dist',      
+    emptyOutDir: true  
   }
 })
