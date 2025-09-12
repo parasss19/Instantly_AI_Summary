72c93ace7f532cfc753448aaee4b929c5b939acd
Paras <mehtaparas1901@gmail.com>
2025-09-12 13:51:02 +0530

    update the image pixels

--- public/scripts/background.js
+++ public/scripts/background.js
@@ -1,12 +1,3 @@
-// chrome.runtime.onInstalled.addListener(() => {
-//     chrome.storage.sync.get(["geminiAPIKey"], (result) => {
-//         if(!result.geminiAPIKey){
-//             chrome.tabs.create({ url: chrome.runtime.getURL("index.html#/options") });
-//         }
-//     })
-// })
-
-
 chrome.runtime.onInstalled.addListener((reason) => {
   if (reason === chrome.runtime.OnInstalledReason.INSTALL) {
     chrome.storage.sync.get(["geminiAPIKey"], (result) => {
@@ -16,5 +7,3 @@ chrome.runtime.onInstalled.addListener((reason) => {
     });
   }
 });
-
-// Optional: Listen for storage changes or add periodic checks if needed
